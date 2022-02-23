179bc3c210acf5dfdf075f20925516facf481899
katiepalumbo <palumbo.katie@gmail.com>
2022-02-23 20:53:25 +0100

    repo setup + header

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <MyHeader />
+    
+  </div>
+</template>
+
+<script>
+import MyHeader from './components/MyHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyHeader,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+      <div class="row header col-1">
+          <img class="img-fluid" src="../assets/logo-green.png" alt="">
+      </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'MyHeader',
+
+}
+</script>
+
+<style scoped lang="scss">
+@import '../style/general.scss';
+
+.container {
+    background-color: #222c36;
+    width: 100vw;
+    
+    .header {
+        justify-content: left;
+        height: 70px;
+        color: #24d34e;
+
+
+        img {
+            border-radius: 50%;
+            height: auto;
+            max-width: 100%;
+            margin:auto;
+            
+        }
+    }
+}
+
+
+</style>
+
